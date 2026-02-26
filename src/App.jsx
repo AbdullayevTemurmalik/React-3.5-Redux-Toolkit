4946f11b348a1780828ff61459a85ee78d2745df
TemurMalik <tmaq77@gmail.com>
2026-02-26 14:42:50 +0500

    Add Store CounterSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset } from "./redux/CounterSlice";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
   return (
     <>
       <Navbar />
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
     </>
   );
 }
