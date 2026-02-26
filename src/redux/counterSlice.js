60b29d0e3749135eb3f4d579b3617f7881a41423
TemurMalik <tmaq77@gmail.com>
2026-02-26 14:31:52 +0500

    Add Store CounterSlice

---
+++ src/redux/counterSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { count: 0 },
+  reducers: {
+    increment: (state) => {
+      state.count++;
+    },
+    decrement: (state) => {
+      state.count--;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+  },
+});
